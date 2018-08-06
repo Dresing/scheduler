d49d1f616ef9135163edbc5c4a1d67e91f6c6c54
Dresing <dresing@live.dk>
2018-08-06 12:35:59 +0200

    Merge

---
+++ tableprinter.py
@@ -0,0 +1,8 @@
+def printTable(table):
+    s = [[str(e) for e in row] for row in table]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+    table = [fmt.format(*row) for row in s]
+    print '\n'.join(table)
+
+printTable(table)
